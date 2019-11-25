1ec440d1068d4d30ba3b5952a7796dd362f57b2a
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-11-25 10:17:03 -0500

    Finished Lock Method

--- Lab-Task-WPF/MainWindow.xaml.cs
+++ Lab-Task-WPF/MainWindow.xaml.cs
@@ -31,15 +31,30 @@ namespace Lab_Task_WPF
         {
             StreamReader sr = new StreamReader(filename);
             int fileSum = 0;
+            int total;
+            string strTotal="";
 
             while (!sr.EndOfStream)
             {
                 fileSum += Convert.ToInt32(sr.ReadLine());
             }
 
-            //Pm;y one Thread at a time allowed to run the following 
+            //only one Thread at a time allowed to run the following 
             lock (myLock)
             {
+                //get total from textBoxAction
+                Action a1 = delegate () { strTotal = this.totalTextBox.Text; };
+                this.totalTextBox.Dispatcher.Invoke(a1); //Worker Thread block
+
+                //Convert the total to an Int
+                total = Convert.ToInt32(strTotal);
+
+                //add file total to whole total
+                total += fileSum;
+
+                //Set total on the TextBox
+                Action a2 = delegate () { strTotal = total.ToString(); };
+                this.totalTextBox.Dispatcher.Invoke(a2); //Worker Thread block
 
             }
 
